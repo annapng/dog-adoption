dea591ecf43eb38a9ced86aeee6d17cd763bfb61
KateRitchie <ritchie_ecen@hotmail.com>
2023-01-30 20:32:36 -0700

    log in sign up pages

--- controllers/index.js
+++ controllers/index.js
@@ -11,4 +11,13 @@ router.get('/', async (req, res) => {
     res.render('landing');
 })
 
+// Added to format sign up handlebar testing
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
