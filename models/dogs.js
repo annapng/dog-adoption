efb319ebdb93e4a241fbe8b1df18f9e14175e9de
annapng <64225221+annapng@users.noreply.github.com>
2023-02-02 19:56:25 -0700

    Update dogs.js

--- models/dogs.js
+++ models/dogs.js
@@ -40,10 +40,6 @@ Dogs.init(
             allowNull: false,
         },
     },
-<<<<<<< Updated upstream
-=======
-          
->>>>>>> Stashed changes
     {
         sequelize,
         timestamps: false,
