a30ab802da520a0f53fed9590231559b8a536606
Jacob Pickron <jpickron30@gmail.com>
2023-02-02 19:50:09 -0700

    got the users selections into an array

--- public/js/postresults.js
+++ public/js/postresults.js
@@ -1,25 +1,54 @@
-// function submitForm() {
-//     let answersSelection = [];
-//     let inputs = document.getElementsByName("size");
-//     for (let i = 0; i < inputs.length; i++) {
-//       if (inputs[i].checked) {
-//         answerSelection.push(inputs[i].value);
-//       }
-//     }
-// }
 
+const answersSelection = [];
 
 function submitForm(event) {
     event.preventDefault();
-    var genderMale = document.querySelector("#male");
-    var genderFemale = document.querySelector("#female");
+    const genderMale = document.querySelector("#male");
+    const genderFemale = document.querySelector("#female");
+    
+
+    
+    if(genderMale.checked) {
+        answersSelection.push(genderMale);
+    }else {
+        answersSelection.push(genderFemale);
+    }
+    console.log(answersSelection);
     if(genderMale.checked && genderFemale.checked) {
         const bothChecked = document.querySelector(".genderOne");
         bothChecked.innerHTML = "ERROR: You can only check one."
         console.log("you can only pick one");
     }
+
+
+
+
+}
+
+function submitSize(event) {
+    event.preventDefault();
+    const sizeSmall = document.querySelector("#small");
+    const sizeMedium = document.querySelector("#medium");
+    const sizeLarge = document.querySelector("#large");
+    if(sizeSmall.checked) {
+            answersSelection.push(sizeSmall);
+        }else if (sizeMedium.checked) {
+            answersSelection.push(sizeMedium);
+        }else {
+            answersSelection.push(sizeLarge);
+        }
+        
+        
+
 }
 
 
+function submitGoodDogs(event) {
+    event.preventDefault();
+    const 
+}
+console.log(answersSelection);
+
 document.querySelector(".signup-form").addEventListener("submit", submitForm)
+document.querySelector(".signup-form").addEventListener("submit", submitSize)
 
